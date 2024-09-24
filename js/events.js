6c6c8b23a160101bbf6b1389a6422aaded268688
RayhanRK <m.rayhankhan01@gmail.com>
2024-09-24 12:25:58 +0600

    js file linked with html files

---
+++ js/events.js
@@ -0,0 +1,27 @@
+//------- Common items-------
+
+// header buttons
+const historyBtn  = document.getElementById('btn-history');
+const donationBtn = document.getElementById('btn-donation');
+
+// Main section and history tab id
+const mainSec    = document.getElementById('main-sec');
+const historySec = document.getElementById('history-page');
+
+
+// Home page 3 cards donation buttons
+const noakhaliBtn = document.getElementById('btn-noakhali');
+const feniBtn     = document.getElementById('btn-feni');
+const quotaBtn    = donationBtn.getElementById('btn-quota');
+
+
+// All cards functionalities :
+// Noakhali card js
+
+
+
+// Feni card js
+
+
+
+// Quota card js
