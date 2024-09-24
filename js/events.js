13e61e5d2b6e75573f8c04b017df8eb9c2d73b86
RayhanRK <m.rayhankhan01@gmail.com>
2024-09-24 19:04:12 +0600

    button toggle color change function added

--- js/events.js
+++ js/events.js
@@ -1,18 +1,53 @@
 //------- Common items-------
 
 // header buttons
-const historyBtn  = document.getElementById('btn-history');
-const donationBtn = document.getElementById('btn-donation');
+const btnHistory  = document.getElementById('btn-history');
+const btnDonation = document.getElementById('btn-donation');
 
 // Main section and history tab id
-const mainSec    = document.getElementById('main-sec');
-const historySec = document.getElementById('history-page');
-
+const mainSection    = document.getElementById('main-sec');
+const historySection = document.getElementById('history-page');
 
 // Home page 3 cards donation buttons
-const noakhaliBtn = document.getElementById('btn-noakhali');
-const feniBtn     = document.getElementById('btn-feni');
-const quotaBtn    = donationBtn.getElementById('btn-quota');
+const noakhaliBut = document.getElementById('btn-noakhali');
+const feniBut     = document.getElementById('btn-feni');
+const quotaBut    = document.getElementById('btn-quota'); 
+
+//------------click events:--------------
+
+btnHistory.addEventListener('click', function(){
+
+    mainSection.classList.add('hidden');
+    historySection.classList.remove('hidden');
+
+    btnHistory.classList.add('bg-[#B4F461]', 'text-[#111111]');
+    btnHistory.classList.remove('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+	
+	
+   btnDonation.classList.remove('bg-[#B4F461]', 'text-[#111111]');
+   btnDonation.classList.add('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+
+})
+
+btnDonation.addEventListener('click', function(){
+
+    mainSection.classList.remove('hidden');
+    historySection.classList.add('hidden');
+
+    btnDonation.classList.add('bg-[#B4F461]', 'text-[#111111]');
+    btnDonation.classList.remove('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+
+    btnHistory.classList.remove('bg-[#B4F461]', 'text-[#111111]');
+
+    btnHistory.classList.add('border-2' ,'border-[#111111B3]','text-[#111111B3]')
+})
+
+
+
+
+
+
+
 
 
 // All cards functionalities :
